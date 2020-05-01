fc1ad461340d29cab0bd5cc24c8fa33eaf0f2b6d
anekenonso <okenwanonsokenneth@gmail.com>
2020-05-01 23:11:41 +0100

    initial commit

---
+++ content/posts/vanillajs/javascript-Dom-101.md
@@ -0,0 +1,22 @@
+---
+title: "Introduction to javaScript DOM 101"
+description: "Detailed Introduction to javaScript Dom API. Understanding javaScript Dom and how manipulate Html with the Dom."
+date: 2020-04-29
+published: true
+canonical_url: true
+tags: ['javascript', 'Dom', 'Api']
+---
+
+The javaScript DOM ( Document Object Model) Api is used to manipulate html document through javaScript.
+
+```javaScript
+
+  const button = document.querySelector('btn);
+
+  button.addEventListener('click', doSomething);
+
+  function doSomething() {
+      console.log('you clicked the button');
+  }
+```
+the above code is an example of how you manipulate a html document using the javaScript dom api. the above code listens for a click event, then executes the function command attached to it.
---
+++ gridsome.config.js
@@ -0,0 +1,45 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here requires a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'okenwa kenneth',
+  siteDescription: 'Personal Blog by Okenwa Kenneth. He is a software developer, he is passionate about cyber security and making the web save for everyone.',
+
+  templates: {
+    Post: '/:title',
+    Tag: '/tag/:id'
+  },
+
+  plugins: [
+    {
+      // Create posts from markdown files
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'Post',
+        path: 'content/posts/**/*.md',
+        refs: {
+          // Creates a GraphQL collection from 'tags' in front-matter and adds a reference.
+          tags: {
+            typeName: 'Tag',
+            create: true
+          }
+        }
+      }
+    }
+  ],
+
+  transformers: {
+    //Add markdown support to all file-system sources
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: [
+        '@gridsome/remark-prismjs'
+      ]
+    }
+  }
+}
