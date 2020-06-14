992607cbe6f7629f6eae7115692884723c7d1b5c
anekenonso <okenwanonsokenneth@gmail.com>
2020-06-14 01:30:11 +0100

    change 404 image to cloudinary link

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -2,7 +2,7 @@
   <div>
     <g-link class="link" to="/">Go Home</g-link>
     <div class="icon">
-      <g-image class="image" src="~/assets/images/404.svg"></g-image>
+      <g-image class="image" src="https://res.cloudinary.com/anekenonso/image/upload/v1592094410/404.svg"></g-image>
     </div>
   </div>
 </template>
