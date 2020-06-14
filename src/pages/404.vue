50ade13a2b49f03d2b60eb09aba2c6731646ae42
anekenonso <okenwanonsokenneth@gmail.com>
2020-06-14 01:13:33 +0100

    created a custom 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <g-link class="link" to="/">Go Home</g-link>
+    <div class="icon">
+      <g-image class="image" src="~/assets/images/404.svg"></g-image>
+    </div>
+  </div>
+</template>
+
+<style >
+.icon {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+}
+
+.link {
+    text-decoration: none;
+}
+
+.image {
+  width: 50%;
+  height: 50%;
+  justify-content: center;
+}
+
+</style>
